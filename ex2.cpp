65b03f77d3be8708d8b8a1f087c0a5353a85f9c6
mannyuk-dev <56549229+mannyuk-dev@users.noreply.github.com>
2021-02-08 06:47:21 +0000

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iostream>
+#include <vector>
+#include <fstream> 
+
+
+//ex2 file writer APwk1
+//else block writes using ios.
+
+using namespace std;
+
+
+
+
+int main() {
+ 
+  char input;
+  string name;
+  do{
+    cout << "Add text? (Y/N)";
+    cin >> input; 
+    if(input == 'N'){
+      break;
+    }
+    //Break loop when N is entered 
+    else{
+      vector<string> list;
+      cout << "Enter name: \n";
+      cin >> name;
+      list.push_back(name);
+
+      ofstream outfile ("list.csv", ios::app);
+      outfile << name << endl;
+      outfile.close();
+    }
+  }
+  while(input == 'Y');
+  
+}
